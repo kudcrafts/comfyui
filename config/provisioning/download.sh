7b814cf04e68d10e149189c5fe688de78e3b4a45
kudcrafts <81409057+kudcrafts@users.noreply.github.com>
2024-05-30 22:25:09 +0800

    Update download.sh

--- config/provisioning/download.sh
+++ config/provisioning/download.sh
@@ -4,3 +4,23 @@ curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec542
 curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o zPDXLxxx-neg.pt https://civitai.com/api/download/models/380277?type=Negative&format=Other
 curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "hoshino_ver2_blue archive_PONY_epoch-000020.safetensors" https://civitai.com/api/download/models/441283
 curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "hoshino-first_ver2_blue archive_PONY_epoch-000020.safetensors" https://civitai.com/api/download/models/441293
+
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "hitori-gotou-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/383887
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "ryou-yamada-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/394952
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "kita-ikuyo-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/394729
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "nijika-ijichi-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/394923
+
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "ena-saitou-s1s2-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/434619
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "rin-shima-s1s2-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/434635
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "aoi-inuyama-s1s2-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/434616
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "nadeshiko-kagamihara-s1s2-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/434626
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "chiaki-oogaki-s1s2-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/434618
+
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "kim-anouk-mei-takanashi-s1-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/462485
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "mahiru-kouzuki-s1-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/462506
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "kano-yamanouchi-s1-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/462484
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "kiui-watase-s1-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/462486
+
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "yukino-yukinoshita-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/396024
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "iroha-isshiki-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/395906
+curl -L -H "Content-Type: application/json" -H "Authorization: Bearer 80d77ec5423b298d563c5c8fb0e75ce4" -o "yui-yuigahama-ponyxl-lora-nochekaiser.safetensors" https://civitai.com/api/download/models/396049
